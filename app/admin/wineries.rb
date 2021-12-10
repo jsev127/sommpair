a65c3f0e915c9866f2bdb20fdca914c049c46301
anonymous anonymous <juan.sevi@kellogg.northwestern.edu>
2021-12-10 06:10:33 +0000

    Whitelist columns in Winery for ActiveAdmin

--- app/admin/wineries.rb
+++ app/admin/wineries.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Winery do
 
+ permit_params :name, :address, :website, :region_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
